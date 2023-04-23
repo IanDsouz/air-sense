0339ea763b876bcb77d487eb81a94a9aae06436d
Ian <ian.dsouza@cityfibre.com>
2023-04-23 11:25:01 +0100

    mc

--- airsense_backend/air_quality/views.py
+++ airsense_backend/air_quality/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import AirQualityReading
 from .serializers import AirQualityReadingSerializer
 
 @api_view(['POST'])
